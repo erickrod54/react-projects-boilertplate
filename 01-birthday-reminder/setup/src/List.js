5f4a7a9f25fd4156e5f36cf8d3f8a9a8445d39be
Erick Rodriguez <erickrod54@gmail.com>
2021-12-23 23:58:13 -0500

    List is the component where i place my JSX, and destructure the data in detail

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const List = () => {
+/**First i pass the people data*/
+const List = ({people}) => {
   return (
     <>
-      <h2>list component</h2>
+       {/** second i map the data*/} 
+      {people.map((person) => {
+        /**third i destructure the data in detail,
+         * in this part i have always to look for 
+         * the 'data' file
+         */
+        const {id, name, age, image} = person;
+
+        /**fourth i assembly the 'JSX' with the data*/
+        return (
+            <article key={id} className='person'>
+                <img 
+                  src={image} 
+                  alt={name}/>
+                <div>
+                  <h4>{name}</h4>
+                  <p>{age} years</p>
+                </div>
+            </article>
+        )
+      })}
+
     </>
   );
 };
