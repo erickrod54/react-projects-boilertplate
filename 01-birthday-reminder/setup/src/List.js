c883f97709e410b8fec4e977b8c3ecaf3d688e18
Erick Rodriguez <erickrod54@gmail.com>
2021-12-25 17:32:48 -0500

    Birthday reminder version 2, same logic that lived once in App js now lives in List component in order to apply remove item feauture

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import data from '../../setup/src/data';
 
 /**First i pass the people data*/
-const List = ({people}) => {
+const List = () => {
+
+  /**-------the same logic that lived in App js, now lives in 'List' 
+   * component in order to apply removeItem feauture -------*/
+  
+  const [ people, setPeople ] = React.useState(data)
+
+  const removeItem = (id) =>{
+    const newPeople = 
+      people.filter((person) => person.id !== id)
+    
+      setPeople(newPeople)
+} 
+
+  /**------------------------------------------------------------*/
+  
   return (
     <>
        {/** second i map the data*/} 
@@ -11,10 +27,11 @@ const List = ({people}) => {
          * the 'data' file
          */
         const {id, name, age, image} = person;
+        
 
         /**fourth i assembly the 'JSX' with the data*/
         return (
-            <article key={id} className='person'>
+            <article key={id} className='person' >
                 <img 
                   src={image} 
                   alt={name}/>
@@ -22,9 +39,29 @@ const List = ({people}) => {
                   <h4>{name}</h4>
                   <p>{age} years</p>
                 </div>
+                <div>
+                <button 
+                      onClick={() => removeItem(id)}
+                        >remove </button> 
+                </div>
             </article>
+            
         )
       })}
+           {/**button to clear the array data 
+           * this inline function give back an
+           * empty array 'setPeople([])'
+          */}
+            <button 
+            onClick={() => 
+            setPeople([])}>Clear Data</button>
+            {/**button to restore data from the array
+             * this inline function give back the 
+             * array with the original data 'setPeople([])'
+             */}
+            <button 
+            onClick={() => 
+            setPeople(data)}>Restore data</button>
 
     </>
   );
