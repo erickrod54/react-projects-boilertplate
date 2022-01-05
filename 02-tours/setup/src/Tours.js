7d3d30435c4214b4868c1679f29e8fa5bf63270e
Erick Rodriguez <erickrod54@gmail.com>
2022-01-04 22:13:10 -0500

    Tours (plural) component is where i map every tour data by id

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+/**This is the tours component, here i'll map
+ * the API to get the tours data -i'll get all
+ * the tours-
+ */
+const Tours = ({tours}) => {
+
+  return (
+  <>
+    <section>
+        <div className='title'>
+          {/**<h2>tours component</h2> */}
+          <h2>our tours</h2>
+          <div className='underline'></div>
+        </div>
+        <div>
+          {/**here is the map all over the API*/}
+          {tours.map((tour) => {
+            /**the component get:
+             *  
+             *    tour by 'id'
+             *    spread rest properties
+             *    with '{...tour}' 
+             *    operator
+             */
+            return <Tour key={tour.id} {...tour}></Tour>
+         })}
+       </div>
+    </section>
+  </>  
+  );
 };
 
 export default Tours;
