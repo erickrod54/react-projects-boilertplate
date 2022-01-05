536f4e4502ceef1a4b4f9ec03e5e57249fa5c6ce
Erick Rodriguez <erickrod54@gmail.com>
2022-01-04 22:09:24 -0500

    Tour component is where tuors data is destructured in detail

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+
+/**this tour component will destructure in detail
+ * the props for each 'Tour' component from 'Tours'
+ */
+
+/**all off these 'id, image, info, price, name', 
+ * are unique 'props' from each json object in the
+ * 'API' -url-
+*/
+const Tour = ({id, image, info, price, name}) => {
+  return (
+  <article className='single-tour'>
+      {/**<h2>tour component</h2> */}
+      <img 
+        src={image} 
+        alt={name} />
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'
+          >${price}</h4>
+        </div>
+       <p>
+          {info}
+        </p>
+        <button className='delete-btn'
+         >Not Interested</button>
+      </footer>
+  </article>  
+  );
 };
 
 export default Tour;
