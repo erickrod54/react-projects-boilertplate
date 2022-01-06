0f8ed00b84ec8119bcecd242ba06296f57ac7b29
Erick Rodriguez <erickrod54@gmail.com>
2022-01-05 22:57:10 -0500

    App js version 2, creating romeTour feature, and drilling it as a prop from App.js to Tour.js

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,67 +5,63 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 
+/**---Tour App --> App js version 2 feature to
+ *  remove a Tour using prop drilling from the 
+ *  place where the state lives (props) to the 
+ *  place where the component trigger the action
+ *  (Tour):
+ *  
+ *  the prop drilling will use the component 'Tour'
+ *  to pass the action 'removeTour':
+ *
+ *     
+ *    App.js > Tours > Tour
+*/
+
 function App() {
-  /**i build two states */
 
-  /**first state for loading 
-   * -this state switch from true to false- */
   const [loading, setLoading] = useState(true)
-
-  /**second state to 'tours', i'll use this state
-   * to fill it with the info from the 'url' in the 
-   * api*/
   const [tours, setTours] = useState([])
 
-  /**here i build the api request with a 
-   * separate function for Promises to get
-   * the data
-   */
+  /**Here i build the removeTour feature, because
+   * here lives tours data, and i'll trigger the 
+   * feature from Tour file
+   * 
+   *  --prop drilling will be:
+   * 
+   *    App.js > Tours > Tour
+  */
+  const removeTour = (id) => {
+    const newTours = 
+    tours.filter((tour) => tour.id !== id);
+
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
-    /**setLoading to 'true' because 
-     * the app will wait for the data
-     */
+    
     setLoading(true)
     
-    /** the try catch, will 'try' to get 
-     * the data, a response await fecth 
-     * the 'url'*/ 
     try {
       const response = await fetch(url);
-      /**in 'tours' i keep the data requested
-       * in a json object
-       */
+      
       const tours = await response.json();
-      /**setLoading to 'false' to show the data
-       * setTours to 'tours'
-      */
+      
       setLoading(false)
       setTours(tours)
-      /**here i prompted in the console the
-       * object, i can watch it in 
-       * 
-       *  JavaConsole > Components 
-       */
       console.log(tours)
       
     } catch (error) {
-      /**if an error occurs 
-       * here will prompted the error
-       */
       setLoading(false)
       console.log(error)
       
     }
   }
-/**in the useEffect i call fetchTours*/
+
   useEffect(() => {
     fetchTours();
   }, [])
 
-  /**A 'conditional rendering' 
-   * if 'loading' is true then 
-   * is Loading rendering 
-  */
   if (loading) {
     return(
       <main>
@@ -78,10 +74,11 @@ function App() {
   <>
      <main>
      <h2>Tours Project Setup</h2>
-     {/**the Tours component render with the 'tours'
-      * value -this value is build in 'Tour' state-
-      */}
-      <Tours tours={tours}/>
+     {/**Here i pass the removeTour feature as prop
+      * ---this technique is called prop drilling---
+     */}
+      <Tours tours={tours} removeTour={removeTour}/>
+      
      </main>
   </>  
   )
