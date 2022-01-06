52c5eeaa572bdd0e657822bfb689c81a8324b887
Erick Rodriguez <erickrod54@gmail.com>
2022-01-05 23:01:34 -0500

    Tours version 2, second drill of removeTour using Tour component

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Tour from './Tour';
 
+/**Tour file version 2, we use the component
+ * to drill removeTour as a prop, to get finally
+ * to 'Tour' file, where will be trigger
+ */
+
 /**This is the tours component, here i'll map
  * the API to get the tours data -i'll get all
  * the tours-
  */
-const Tours = ({tours}) => {
+const Tours = ({tours, removeTour }) => {
 
   return (
   <>
@@ -25,7 +30,11 @@ const Tours = ({tours}) => {
              *    with '{...tour}' 
              *    operator
              */
-            return <Tour key={tour.id} {...tour}></Tour>
+            return <Tour key={tour.id} 
+                        {...tour} 
+                /**Here i pass as a prop removeTour 
+                 * feature */            
+                          removeTour={removeTour}></Tour>
          })}
        </div>
     </section>
