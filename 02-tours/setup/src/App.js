6549c71ee910688c33479f7d3abbcfa8e0ede475
Erick Rodriguez <erickrod54@gmail.com>
2022-01-06 23:27:33 -0500

    App js version 3, adding new conditional rendering to show empty list of Tours and a button to quest Tours data

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,7 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 
-/**---Tour App --> App js version 2 feature to
+/**---Tour App --> App js version 3 feature to
  *  remove a Tour using prop drilling from the 
  *  place where the state lives (props) to the 
  *  place where the component trigger the action
@@ -16,6 +16,10 @@ const url = 'https://course-api.com/react-tours-project'
  *
  *     
  *    App.js > Tours > Tour
+ * 
+ * this version 3 has a new 'conditional render'
+ * to notifify when there no more tours, and a 
+ * button to request again the tours data
 */
 
 function App() {
@@ -66,6 +70,25 @@ function App() {
     return(
       <main>
         <Loading />
+      </main>
+    )
+  }
+
+/**Here i add a 'conditional rendering' to render
+ * a notice when theres not tours left, and a button
+ * to request again the tours data
+*/
+  if (tours.length === 0) {
+    return(
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button 
+            className='btn'
+            onClick={ () => fetchTours()}>
+            Request Tours
+          </button>
+        </div>
       </main>
     )
   }
