b1de3939a2eaeead2800738161b91beb660a25b2
Erick Rodriguez <erickrod54@gmail.com>
2022-01-09 00:00:37 -0500

    this is the version 1 of review app

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Review from './Review';
-
-import {FaGithubSquare} from 'react-icons/fa'
-
 function App() {
   return (
   <>
     <h2>reviews project setup</h2>
-    <FaGithubSquare className='icon'/>
-  </>  
+    <section className='container'>
+      <div className='title'>
+        <h2>our reviews</h2>
+        <div className='underline'></div>
+      </div>
+      <Review />
+    </section>
+  
+  </>
+    
   );
 }
 
