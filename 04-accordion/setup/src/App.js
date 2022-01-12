b408baf4edf8134d8432e118730ab206150e628c
Erick Rodriguez <erickrod54@gmail.com>
2022-01-11 22:54:15 -0500

    Accordion App version 1 - i map all the data in the SingleQuestion component

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,36 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
+/**Accordion App version 1 - in this file:
+ *  i map the array of questions, and i use the 
+ * component:
+ * ---- '<SingleQuestion/>' -----
+ * 
+ * to show every
+ * single question - */
+
 function App() {
-  return <h2>accordion project setup</h2>;
+
+const [ questions, setQuestions ] = useState(data)
+
+  return (
+    <>
+      <h2>accordion project setup</h2>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {
+            questions.map((question) =>{
+              return <SingleQuestion key={question.id} {...question} />
+            })
+          }
+        </section>
+      </div>
+    
+    </>
+    
+  );
 }
 
 export default App;
