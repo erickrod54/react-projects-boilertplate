d3e0821713f3a01fa9d37f181099226b97c408e7
Erick Rodriguez <erickrod54@gmail.com>
2022-01-13 18:05:43 -0500

    Menu App version 1,building states, filter categories, and render Categories and Menu Component

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,54 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+/**Menu App version 1, features:
+ *    
+ *  building states - menuItems, setMenuItems -
+ * ---i'll map the menuItems from 'Menu' component--
+ * 
+ *  
+ */
 function App() {
-  return <h2>menu project setup</h2>;
+
+  /**the state to managae the items */
+  const [ menuItems, setMenuItems ] = useState(items);
+
+  /**the state to manage the categories */
+  const [ categories, setCategories] = useState([]);
+
+  /**here i build the filterItems functionality
+   * to filter by 'category' and by 'item'
+   */
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category
+    === category)
+  /**here using 'setMenuItems' state, i set the 'items'
+   * with the 'newItems' object -object already 
+   * filtered-
+  */
+    setMenuItems(newItems)
+  }
+
+  return (
+  <>
+  <main>
+    <h2>menu project setup</h2>
+    <section className='menu section'>
+      <div className='title'>
+        <h2>Our menu</h2>
+        <div className='underline' />
+      </div>
+      {/**here i render Categories and Menu component*/}
+
+      {/**i pass filterItems as a prop to use it in
+       * 'Categories' component
+       */}
+        <Categories filterItems={filterItems} />
+        <Menu items={menuItems}/>
+    </section>
+  </main>
+  </>  
+  );
 }
 
 export default App;
