beb418bd46cfbcb6cb776592e2af686cf092423d
Erick Rodriguez <erickrod54@gmail.com>
2022-01-13 18:09:30 -0500

    Menu App version 1 - building Categories Component, manual filtering, pending dynamic approach

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+/**Menu App version 1 - features:
+ * 
+ *    filter 'Categories', this is a 
+ *    'Manual Approach', for version 2
+ *    will be dynamicly
+ */
+
+const Categories = ({ filterItems }) => {
+  return (
+  <div className='btn-container'>
+    {/**<h2>categories component</h2> */}
+    <button 
+        className='filter-btn' 
+      /**i manually filter the 'category' by string */
+        onClick={() => filterItems('Christmas food')}>
+      Christmas Food
+    </button>
+  </div>
+    
+  );
 };
 
 export default Categories;
