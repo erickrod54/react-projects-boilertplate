bd7e496f1523f0f5a549b22bc5008325cce62de3
Erick Rodriguez <erickrod54@gmail.com>
2022-01-14 21:33:28 -0500

    Menu app version 1 - destructure in detail every item for the MenuItem-

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import menu from './data';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+/**Menu App version 1 - Here in 'Menu' component
+ * i destructure in detail the 'items' for every 
+ * 'item' in the 'menuItem' 
+*/
+
+const Menu = ({ items }) => {
+  return (
+  <>
+    <h2>menu component</h2>
+    <div className='section-center'>
+      {
+        /**here i map the the 'menu' array */
+        items.map((menuItem) =>{
+          const {id, title, img, desc, price } = menuItem;
+
+          return(
+            /**by id, img, title, price, and desc
+             * every item
+             */
+            <article key={id} className='menu-tem'>
+              <img 
+                src={img} 
+                alt={title} 
+                className='photo'/>
+              <div className='item-info'>
+                <header>
+                  <h4>{title}</h4>
+                  <h4 className='price'>${price}</h4>
+                </header>
+                <p className='item-text'>{desc}</p>
+              </div>
+            </article>
+          )
+        })
+      }
+    </div>
+  </>
+    
+  );
 };
 
 export default Menu;
