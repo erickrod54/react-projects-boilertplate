01edc7ca1289e8cf0f8bc3035daa41afd53e3216
Erick Rodriguez <erickrod54@gmail.com>
2022-01-24 22:24:58 -0500

    Lorem Ipsum App version 1 - Features: building states, building a basic input, loopbacking the handleSubmit, styling the App

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,64 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+
+  /**Lorem ipsum App version 1 - 
+   *              ------>building states.
+   * 
+   *              ------>building a basic input.
+   *                    -to get the paragraph number-
+   * 
+   *              ------> creating the 'handleSubmit'
+   *                      and loopback with a console
+   *                      log to test response after
+   *                      prevent defaul behavior.
+   * 
+   *              ------->styling the lorem ipsum App
+   *                          
+  */
   
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello world')
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+    <>
+      {/**all the 'JSX' and applied styles:
+       * 
+       *  -->section-center where the app
+       *     is gonna show up
+       *  -->'amount' is the style for paragraphs title.
+       * 
+       *  -->input type'number' where the amount is 
+       *     gonna be entered.
+       * 
+       *  -->button to generate paragraph quantity.
+       * 
+       *  -->article is where the lorem ipsum is gonna be
+       *    generated -this version manually to set some
+       *    kind of style but in the next version will be
+       *    mapped to do it dynamicly-.
+       *  
+       */}
+      <h2>lorem ipsum project setup</h2>
+      <section className='section-center'>
+        <h3>tired of boring lorem ipsum?</h3>
+        <form className='lorem-form' onSubmit={handleSubmit}>
+          <label htmlFor='amount'>
+            paragraphs:
+          </label>
+          <input type='number' name='amount' id='amount' value={count} onChange={(e) => setCount(e.target.value)}/>
+        <button type='submit' className='btn'>generate</button>
+        </form>
+      <article className='lorem-text'>
+        <p>Jelly sweet roll jelly beans bi, blog copper mug forage kitsch roof party pickled hammock kale chips tofu. Etsy shoreditch 8-bit microdosing, XOXO viral butcher banh mi humblebrag listicle woke bicycle rights brunch before they sold out ramps. Twee shabby chic taiyaki flannel, enamel pin venmo vape four loko. Hexagon kale chips typewriter kitsch 8-bit organic plaid small batch keffiyeh ethical banh mi narwhal echo park cronut.`,</p>
+      </article>
+      </section>
+    </>
     )
 }
 
