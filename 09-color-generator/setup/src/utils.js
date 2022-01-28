ad3e8806fb754a2c1e3b58078c5148ff79635154
Erick Rodriguez <erickrod54@gmail.com>
2022-01-27 19:46:21 -0500

    Color Generator app version 3 - utils js file contains an existing functionality to convert rgbToHex, credits for stackoverflow https://stackoverflow.com/users/133/luke-girvin

--- 09-color-generator/setup/src/utils.js
+++ 09-color-generator/setup/src/utils.js
@@ -1,3 +1,13 @@
+/**
+ * As i need to convert a rgb value to hex in order
+ * to show every hex value on every color box, i use
+ * a function to do it
+ * 
+ * ---converts passing 'rgb' to 'hex'
+ * 
+ * -----credits for stackoverflow website----
+ */
+
 function componentToHex(c) {
   var hex = c.toString(16);
   return hex.length == 1 ? '0' + hex : hex;
