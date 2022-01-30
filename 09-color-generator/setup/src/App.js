40b50233cc7b35ad5d48fe148a8259dd593674f2
Erick Rodriguez <erickrod54@gmail.com>
2022-01-29 19:03:38 -0500

    color Generator App - version 5 - Features: creating a new value for the List state and mimic a default value- can be improve-

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -3,40 +3,32 @@ import SingleColor from './SingleColor'
 
 import Values from 'values.js'
 
-/**Color Generator App - version 4 - Features:
+/**Color Generator App - version 5 - Features:
  * 
- *    ---> i 'setError' to false in the
- *          try-catch
+ *    ---> i create a new value for the 'List' state
+ *         and is posibble to get it improve 
  *         
  */
 
 function App() {
-  /**this is the 'state' for the color */
+  
   const [ color, setColor ] = useState('');
 
-  /**this is the state for the error, in case
-   * that the entry is wrong, will contain a state 
-   * for error messages*/
   const [error, setError ] =useState(false);
 
-  /**this state will holbe filled out with the 
-   * colors data*/
-  const [list, setList] = useState([])
+  /**modifying the list state value to mimic a 
+   * set default and modifying the number a can 
+   * can more o less colors indeed can be an 
+   * improvement
+   */
+  const [list, setList] = useState(new Values('#f15025').
+  all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /**whith this try-catch i handle the error*/
     try {
       
-      /**this makes reference to a library
-       * that has an API with the 'colors'
-       * 
-       * -- https://
-       *   github.com/noeldelgado/values.js
-       * 
-       */
-      /**doing this method i get 20 colors
-       * plus the color i give 21*/
       let colors = new Values(color).all(10);
       setList(colors);
       setError(false)
