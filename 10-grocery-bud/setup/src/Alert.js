a6ea9a0a9b426607df5dff08e2181e733738621a
Erick Rodriguez <erickrod54@gmail.com>
2022-01-31 19:22:47 -0500

    Grocery App version 2 - Alert Component - Features: drilling props type, msg, removeAlert (this last contain showAlert functionality)to build here cleanup function

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,34 @@
 import React, { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>alert component</h2>
+/**Grocery App version 2 - Features:
+ *      -->Building alert component
+ */
+
+
+/**drilling the props that i need from the alert
+ * state key object in App js, and drilling also
+ * 'removeAlert' prop that has 'showAlert' 
+ * functionality
+ */
+const Alert = ({ type, msg, removeAlert }) => {
+
+  /**building the cleanup function 
+   * -to clean the alert- */
+  useEffect(() => {
+    const timeout = setTimeout(() =>{
+      removeAlert()
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return (
+    <>
+      {/**<h2>alert component</h2> */}
+      {/**i pass a type and a message */}
+      <p className={`alert alert-${type}`}>{msg}</p>
+    </>
+    
+  )
 }
 
 export default Alert
