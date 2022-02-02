aab12f68189eb33108abd467bb3492d332a02065
Erick Rodriguez <erickrod54@gmail.com>
2022-02-01 22:21:29 -0500

    Grocery App version 3 - Alert Component - Features: Getting drilled list value to set up a dependency array

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react'
+import List from './List'
 
-/**Grocery App version 2 - Features:
- *      -->Building alert component
+/**Grocery App version 3 - Features:
+ * 
+ *      --> i get the 'list' value from App js to 
+ *          'setTimeOut' by the 'list' as an array
+ *          of dependencies -to fix the bug where 
+ *         the timeOut change by the type of alert-
+ * 
  */
 
 
-/**drilling the props that i need from the alert
- * state key object in App js, and drilling also
- * 'removeAlert' prop that has 'showAlert' 
- * functionality
+/**
+ * here i destructure the 'list' value as a prop
  */
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = ({ type, msg, removeAlert, list }) => {
 
   /**building the cleanup function 
    * -to clean the alert- */
@@ -19,7 +23,9 @@ const Alert = ({ type, msg, removeAlert }) => {
       removeAlert()
     }, 3000)
     return () => clearTimeout(timeout)
-  }, [])
+    /**here i set the 'list' value as a dependency
+     * array*/
+  }, [list])
 
   return (
     <>
