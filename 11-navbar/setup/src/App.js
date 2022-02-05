daa5b061b1e467322da20bad418f7bc499ece8c8
Erick Rodriguez <erickrod54@gmail.com>
2022-02-05 05:38:04 -0500

    NavBar App version 1 - Features: setting up NavBar component in order to be render

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -3,7 +3,8 @@ import Navbar from './Navbar'
 function App() {
   return (
     <>
-      <h2>navbar project setup</h2>
+      {/**<h2>navbar project setup</h2> */}
+      <Navbar />
     </>
   )
 }
