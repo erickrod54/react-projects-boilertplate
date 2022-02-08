a9410ab09bb792a5515ce8e8c9f2966dcaea02e7
Erick Rodriguez <erickrod54@gmail.com>
2022-02-07 19:30:22 -0500

    NavBar App version 3 - App js file - Features: importing SideBar Component

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from './Navbar'
+import './SideBar'
+import SideBar from './SideBar'
+
+/** NavBar app version 3 - Features:
+ * 
+ *      -----> importing 'SideBar' Component
+ * 
+ */
+
 function App() {
   return (
     <>
       {/**<h2>navbar project setup</h2> */}
       <Navbar />
-    </>
+      <SideBar />
+    </> 
   )
 }
 
