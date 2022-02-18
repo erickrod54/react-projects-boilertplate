bfb0c7268ef9ad49ce3788d1abd2cc6200bbba6f
Erick Rodriguez <erickrod54@gmail.com>
2022-02-18 16:11:51 -0500

    SideBar-Modal app version 1 - Home Component - Features: Structuring and placing FaBars to toggle SideBar Component, Placing the button to show the modal, Styling sidebar-toggle

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,27 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
 
+/**
+ * SideBar-Modal app version 1 - Home Component - Features:
+ *          ---->Structuring placing '<FaBars />' to toggle 
+ *               the 'SideBar' Component  
+ *          ---->Placing the button to show modal
+ *          ---->Styling: 'sidebar-toggle' -for SideBar button-
+ *               and 'btn' -to show modal-
+ */
+
 const Home = () => {
-  return <h2>home component</h2>
+  return(
+    <>
+      {/**<h2>home component</h2> */}
+      <main>
+        <button className='sidebar-toggle'>
+          <FaBars />
+        </button>
+        <button className='btn'>show modal</button>
+      </main>
+    </>
+  )
 }
 
 export default Home
