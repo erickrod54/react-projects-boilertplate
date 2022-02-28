ec877eba1fd52fa04c4d03256837daec5094de0a
Erick Rodriguez <erickrod54@gmail.com>
2022-02-28 17:04:55 -0500

    SideBar-Modal app version 1 - Modal Component - Features: Placing a modal container and a FaTimes icon to close the modal, Styling the Modal component to show -toggle styling class-

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,7 +1,37 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+
+/**
+ * SideBar-Modal app version 1 - Modal Component - Features:
+ *          ----> Placing a 'modal container' and a 
+ *                '<FaTimes />' icon to close the modal 
+ *          ----> Styling the 'Modal' component:
+ * 
+ *                -->`modal-overlay` with a `show-modal` toggle
+ *                    class -to style and show the modal and 
+ *                    its content-. 
+ * 
+ *                -->`modal conrainer` the style for
+ *                    the modal container.
+ *  
+ *                -->'close-modal-btn' the 
+ *                    style for the close modal button
+ */
+
 const Modal = () => {
-  return <h2>Modal</h2>
+  return(
+    <>
+      {/** <h2>Modal</h2> */}
+    <div className={`modal-overlay `}> 
+      <div className='modal-container'>
+        <h3>modal container</h3>
+        <button className='close-modal-btn'>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+    </>
+  )
 }
 
 export default Modal
