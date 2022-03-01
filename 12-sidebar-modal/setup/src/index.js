9798cf40509c1dd0f8f7118cbbbba26cf0c2232a
Erick Rodriguez <erickrod54@gmail.com>
2022-03-01 18:12:58 -0500

    SideBar-Modal app version 2 - index js file -Features: Wrapping the whole app with <AppProvider> imported from context js in order to pass values top-down the children

--- 12-sidebar-modal/setup/src/index.js
+++ 12-sidebar-modal/setup/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { AppProvider } from './context'
 import App from './App'
+
+/**SideBar-Modal app version 2 - Index js file - Features:
+ *      -->Wrapping the whole 'App' with <AppProvider>
+ * 
+ * Note: this after building the context and the provider in
+ * context js file -make sure that always 'children' are 
+ * included in the provider-
+ */
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
