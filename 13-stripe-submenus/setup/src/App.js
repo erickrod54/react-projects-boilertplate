188077d4cfea5d78042971efc29a9da642b1dc5e
Erick Rodriguez <erickrod54@gmail.com>
2022-03-05 23:22:35 -0500

    Stripe-submenu app version 1 - App js file - Features: Mounting all the Components

--- 13-stripe-submenus/setup/src/App.js
+++ 13-stripe-submenus/setup/src/App.js
@@ -3,10 +3,21 @@ import Navbar from './Navbar'
 import Hero from './Hero'
 import Sidebar from './Sidebar'
 import Submenu from './Submenu'
+
+/**Stripe-submenu app version 1 - App js file - Features:
+ * 
+ *          -->Mounting all the Components 
+ */
+
 function App() {
+  
   return (
     <>
-      <h2>stripe submenus setup</h2>
+      {/**<h2>stripe submenus setup</h2> */}
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
     </>
   )
 }
