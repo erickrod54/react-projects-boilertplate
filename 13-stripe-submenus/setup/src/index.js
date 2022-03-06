bad0792e1616fae2ad5a93e0f7e493e0c5f7b4aa
Erick Rodriguez <erickrod54@gmail.com>
2022-03-06 15:38:01 -0500

    Stripe-submenu app version 1 - index hs file - Features: importing AppProvider and wrapping the app <App> to pass everything, states adn features to the children

--- 13-stripe-submenus/setup/src/index.js
+++ 13-stripe-submenus/setup/src/index.js
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { AppProvider } from './context'
+
+/**Stripe-submenu app version 1 - index js file - Features:
+ * 
+ *          -->Importing 'AppProvider' and wrapping the app 
+ *             '<App>' to pass everything states and features
+ *              to the childreen.
+ * 
+ */
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
