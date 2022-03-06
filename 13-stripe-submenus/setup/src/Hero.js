deba62b83f551c21929f5434047218d03ed808e8
Erick Rodriguez <erickrod54@gmail.com>
2022-03-06 15:31:06 -0500

    Stripee-submenu app version 1 - Hero Component - Features: Testing access to the two states and four functions created at context js

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 import phoneImg from './images/phone.svg'
 
+/**Stripe-submenu app version 1 - Hero Component - Features:
+ * 
+ *          -->Testing the access to the two states, and four
+ *             functions
+ * 
+ * Notes: have access to four functions closeSidebar, 
+ * closeSubmenu, openSidebar, openSubmenu, and 
+ * two states isSubmenuOpen, isSidebarOpen 
+ */
+
 const Hero = () => {
+  const data = useGlobalContext();
+  console.log(data)
   return <h2>hero component</h2>
 }
 
