0f1eaf110cb6b2c42313fd287e264d650eb5052b
Erick Rodriguez <erickrod54@gmail.com>
2022-03-05 23:24:57 -0500

    Stripe-submenu app version 1 - context js file - Features:Building Appcontext, Building AppProvider, Building states and functions related to Modal and SideBar Components

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,61 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+/**Stripe-submenu app version 1 - context js file - Features:
+ * 
+ *          -->Building 'AppContext' to build the custom hook.
+ *          -->Building 'AppProvider' that contains states
+ *             realted to the Submenu -that is a modal- and
+ *             and state related to 'sidebar'.
+ *          -->Building reference functions related to
+ *             close and open both 'Modal' and 'SideBar'.
+ * 
+ */
+
+/**this is the AppContext to build custom hook*/
+const AppContext = React.createContext();
+
+/**
+ * here i build the AppProvider to make states and 
+ * functions to toggle the 'Sidebar' and 'Submenu' */
+export const AppProvider = ({ children }) => {
+
+    const [ isSidebarOpen, setIsSidebarOpen ] = useState(true);
+    const [ isSubmenuOpen, setIsSubmenuOpen ] = useState(true);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+    const openSubmenu = () => {
+        setIsSubmenuOpen(true)
+    }
+
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false)
+    }
+    /**here in the 'AppContext.Provider' value i pass 
+     * everything states and features to toggle 'Sidebar' and
+     * 'Submenu' */
+    return (
+    <AppContext.Provider value={{
+                            isSidebarOpen, 
+                            isSubmenuOpen,
+                            openSidebar,
+                            closeSidebar,
+                            openSubmenu,
+                            closeSubmenu        
+                                 }}>
+        {children}
+    </AppContext.Provider>
+    )
+}
+
+//custom hook - to use props and features where i need them-
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
