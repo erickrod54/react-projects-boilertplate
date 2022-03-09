437b7006a61b7b45859d6590b739045fee99cb84
Erick Rodriguez <erickrod54@gmail.com>
2022-03-08 21:23:03 -0500

    Stripe-submenu app version 2 - SubmMenu Component - Features: Importing useGlobalContext and using isSubmenuOpen state to conditionally render a style submenu show or submenu

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,30 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { useGlobalContext } from './context'
+
+/**Stripe-submenu app version 2 - SubMenu component - Features:
+ * 
+ *          -->Importing 'useGlobalContext' and using 
+ *             'isSubmenuOpen' to display a style 
+ *             'submenu show' or 'submenu'  
+ * 
+ * Note: from this version forward i will tackle the NavBar
+ * Modal
+ */
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
+  /**here i destructure 'isSubmenuOpen' */
+  const { isSubmenuOpen } = useGlobalContext()
+  return (
+  <>
+  {/**<h2>submenu component</h2> */}
+  
+  {/** here is the Submenu Component*/}
+    <aside className={`${isSubmenuOpen ? 
+      'submenu show' : 'submenu'}`}>
+        submenu  
+      </aside>
+  </>
+  )
 }
 
 export default Submenu
