b002165c2f2ee592cadc877e831e6102abc1c2fc
Erick Rodriguez <erickrod54@gmail.com>
2022-03-11 18:50:36 -0500

    Stripe-submenu app version 4 - Submenu Component - Features: Destructuring the page value from find method on context js file, Mapping the sublinks depending on the page, Using the useEffect to change dinamycly the width of the Submenu depending on the number of links

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,34 +1,41 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
-/**Stripe-submenu app version 3 - SubMenu component - Features:
+/**Stripe-submenu app version 4 - SubMenu component - Features:
  * 
- *          -->Destructuring and getting the 'location' from 
- *            'context' js
- *          -->Building an 'useEffect' to trigger deppending 
- *             on the 'location'
+ *          -->Destructuring the page value from find mehtod on 
+ *             context js, and the links i'll fill it here in 
+ *             Submenu Component.
  * 
- * Note: after this implementation i should see the submenu
- * modal moving dynamicly when the mouseOnOver any page 
- * button
+ *          -->Mapping the 'sublinks' depending on the page.
+ * 
+ *          -->Using the useEffect to change dinamycly the 
+ *            width of the 'Submenu' depending on the number  
+ *            of 'sublinks'.
+ * 
  */
 
 const Submenu = () => {
-  /**here i destructure 'isSubmenuOpen' */
-  const { isSubmenuOpen, location } = useGlobalContext()
+  /**here i destructure 'page'*/
+  const { isSubmenuOpen, location, page: { page, links }} = useGlobalContext()
   const container = useRef(null)
+  const [ columns, setColumns ] = useState('col-2')
 
   useEffect(() => {
-    /**here i create submenu to keep the current 
-     * value of the container
-     */
+    setColumns('col-2')
     const submenu = container.current;
-    /**then i destructure center and bottom on location */
     const { center, bottom } = location;
     /**here i set insline style for center and bottom */
     submenu.style.left = `${center}px`
     submenu.style.top = `${bottom}px`
-  }, [location])
+
+    if (links.length === 3) {
+      setColumns('col-3')
+    }
+    if (links.length > 3) {
+      setColumns('col-4')
+    }
+  }, [location, links])
 
   return (
   <>
@@ -37,7 +44,18 @@ const Submenu = () => {
   {/** here is the Submenu Component*/}
     <aside className={`${isSubmenuOpen ? 
       'submenu show' : 'submenu'}`} ref={container}>
-        submenu  
+        <h4>{page}</h4>  
+        {/**i'll focus on col-2 that will change the width*/}
+        {/**refers to sublinks in data js */}
+        <div className={`submenu-center ${columns}`}>
+          {links.map((link, index) =>{
+            const { label, icon, url } = link;
+            return <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          })}  
+        </div>
       </aside>
   </>
   )
