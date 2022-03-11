8fae057bc9eb396b4016e2c261c4f3acffb35649
Erick Rodriguez <erickrod54@gmail.com>
2022-03-10 21:40:07 -0500

    Stripe-submenu app version 3 - SubMenu Component - Features: Destructuring and getting Location from context js file, Building an useEffect to trigger depending on the Location

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,26 +1,42 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
-/**Stripe-submenu app version 2 - SubMenu component - Features:
+/**Stripe-submenu app version 3 - SubMenu component - Features:
  * 
- *          -->Importing 'useGlobalContext' and using 
- *             'isSubmenuOpen' to display a style 
- *             'submenu show' or 'submenu'  
+ *          -->Destructuring and getting the 'location' from 
+ *            'context' js
+ *          -->Building an 'useEffect' to trigger deppending 
+ *             on the 'location'
  * 
- * Note: from this version forward i will tackle the NavBar
- * Modal
+ * Note: after this implementation i should see the submenu
+ * modal moving dynamicly when the mouseOnOver any page 
+ * button
  */
 
 const Submenu = () => {
   /**here i destructure 'isSubmenuOpen' */
-  const { isSubmenuOpen } = useGlobalContext()
+  const { isSubmenuOpen, location } = useGlobalContext()
+  const container = useRef(null)
+
+  useEffect(() => {
+    /**here i create submenu to keep the current 
+     * value of the container
+     */
+    const submenu = container.current;
+    /**then i destructure center and bottom on location */
+    const { center, bottom } = location;
+    /**here i set insline style for center and bottom */
+    submenu.style.left = `${center}px`
+    submenu.style.top = `${bottom}px`
+  }, [location])
+
   return (
   <>
   {/**<h2>submenu component</h2> */}
   
   {/** here is the Submenu Component*/}
     <aside className={`${isSubmenuOpen ? 
-      'submenu show' : 'submenu'}`}>
+      'submenu show' : 'submenu'}`} ref={container}>
         submenu  
       </aside>
   </>
