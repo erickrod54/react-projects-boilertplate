14ab2d9ec5a7a3d1104ccc92b61e9aece8a6d869
Erick Rodriguez <erickrod54@gmail.com>
2022-03-11 18:46:02 -0500

    Stripe-submenu app version 5 - NavBar Component - Features: Building handleSubmenu feature to close the submenu conditionally depending on the existence OnMouseOver of link-btn style

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -3,16 +3,15 @@ import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
 import { useGlobalContext } from './context'
 
-/**Stripe-submenu app version 4 - NavBar Component - Features:
+/**Stripe-submenu app version 5 - NavBar Component - Features:
  * 
- *          -->Fixing const 'page = e.target.textContext'
- *             that is wrong, is:
- *          
- *              const 'page = e.target.textContent'
+ *          -->Building 'handleSubmenu' feature to close
+ *             the submenu conditionally dependending 
+ *             on the existence 'OnMouseOver' of
+ *             'link-btn'
  * 
- * Note: this is important, this is the 'page' text that i 
- * get from my buttons, and compare with my data to find
- * a match and get the links for that 'page'
+ * Note: "link-btn" the buttons class will be use
+ * handle 'closeSubmenu' 
  */
 
 const Navbar = () => {
@@ -22,24 +21,25 @@ const Navbar = () => {
   
   const displaySubmenu = (e) => {
     const page = e.target.textContent;
-    /**this tempBtn give me the exactly position of the button
-     * on Over*/
     const tempBtn = e.target.getBoundingClientRect();
-    /**now i get the center-this to place the 'Submenu'-
-     * left and right value come from the object*/
-    const center = (tempBtn.left + tempBtn.right) / 2
-    /**then i get the bottom to place the Submenu 3 px under
-     * the button*/
+    const center = (tempBtn.left + tempBtn.right) / 2;
     const bottom = tempBtn.bottom - 3;
-    /**i call openSubmenu and passing page, center and
-     *  bottom */
+    
     openSubmenu( page, { center, bottom })
   } 
+
+  const handleSubmenu = (e) => {
+    /**here i have to check and apply 'closeSubmenu'
+     * to everything that does not contains 'link-btn'
+     * -my buttons class-*/
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu()
+    }
+  }
   return (
   <>
-    {/**<h2>navbar component</h2> */}
-    {/**Here is the NavBar Component */}
-    <nav className='nav'>
+
+    <nav className='nav' onMouseOver={handleSubmenu}>
       {/**here i placed logo and Bars */}
       <div className='nav-center'>
         <div className='nav-header'>
