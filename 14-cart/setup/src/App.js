a77fb37e5328e50731b15951898c450f6cb49d8c
Erick Rodriguez <erickrod54@gmail.com>
2022-03-12 20:24:41 -0500

    Cart app version 2 - App js file - Features: Importing useGlobalContext and destructuring loading value in order to be use

--- 14-cart/setup/src/App.js
+++ 14-cart/setup/src/App.js
@@ -6,14 +6,25 @@ import Navbar from './Navbar'
 import CartContainer from './CartContainer'
 // items
 
+/**Cart app version 2 - App js file - Features:
+ *            ---> Importing 'useGlobalContext' and destructuring
+ *                 'loading' value in order to be use
+ *                       
+ * 
+ * Note: this app has already set a boilerplate in order
+ * to work */
+
 function App() {
-  // if (loading) {
-  //   return (
-  //     <div className='loading'>
-  //       <h1>Loading...</h1>
-  //     </div>
-  //   )
-  // }
+  /**here i destructure loading value */
+  const { loading } = useGlobalContext()
+  /**here is the conditional rendering to use 'loading' */
+   if (loading) {
+     return (
+       <div className='loading'>
+         <h1>Loading...</h1>
+       </div>
+     )
+    }
   return (
     <main>
       <Navbar />
