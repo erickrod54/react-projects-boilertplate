7f6154af362681ac596f5a8398ce196304ea731e
Erick Rodriguez <erickrod54@gmail.com>
2022-03-17 19:41:33 -0400

    Cart app version 9 - reducer js - Features: Refactoring DECREASE and INCREASE actions unifying code for both

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,12 +1,10 @@
-/**Cart app version 8- reducer js - Features:
- *            --->Building 'LOADING' action to be displayed
- *                setting it up by true.
- *            --->Building 'DISPLAY_ITEMS' to show the
- *                'action.payload'  that is the cart   
+/**Cart app version 9- reducer js - Features:
+ * 
+ *            --->Refactoring 'DECREASE' and 'INCREASE'
+ *                actions unifying code for both              
  *                       
  * 
- * Note: context js was implemented and here in the 
- * reducer the actions are build
+ * Note: all the code to the previous feature was removed
  * */
 
 const reducer = (state, action) => {
@@ -22,59 +20,14 @@ const reducer = (state, action) => {
              action.payload )}
     }
 
-    /**here i build increase feature */
-    if (action.type === 'INCREASE') {
-        /**first i set the return (line 37, line 43):
-         *  
-         *  >> let tempCart = []
-         *  >> return { ...state: temCart}
-         * 
-         * , and then build the map -so be easy to follow on-
-         */
-        let tempCart = state.cart.map((cartItem) => {
-            if (cartItem.id === action.payload) {
-                return {...cartItem, amount: cartItem.amount + 1 }
-            }
-            return cartItem
-        });
-        return {...state, cart: tempCart} 
-    }
-    /**here i build decrease feature -is a copy of increase -*/
-    if (action.type === 'DECREASE') {
-        let tempCart = state.cart.map((cartItem) => {
-            if (cartItem.id === action.payload) {
-                            /**instead of adding, decrease 1 */
-                return {...cartItem, amount: cartItem.amount - 1 }
-            }
-            return cartItem
-            /**after decrese i filter, when 'amount' is not
-             * equal to 0, i'll take off the cartItem returning
-             * the 'tempCart' -this feature is alike 'remove'-*/
-        }).filter((cartItem) => cartItem.amount !== 0)
-        return {...state, cart: tempCart} 
-    }
     if (action.type === 'GET_TOTALS') {
-        /**i'll build the reduce in order to return an 
-         * object with total and amount values, this is the 
-         * way to set up, i defined as let to fix the digits
-         * for the total:
-         * 
-         * >> let { total, amount } = state.cart.reduce(()=> {},{})
-         * 
-         * this first bracket '{' is the function body:
-         * 
-         * >> for the values check in 'data js'
-         * 
-         * after the comma ',' i return the object
-         */
+        
         let { total, amount } = state.cart.reduce((cartTotal, cartItem)=> {
             const { price, amount } = cartItem
-        /**to calculate the total for 'cartContainer' i use
-         * a separate variable*/
+       
         const itemTotal = price * amount;
         cartTotal.total += itemTotal;    
-            /**if i console log it, definitive helps to get it */
-            //console.log(price, amount)
+         
             cartTotal.amount += amount;
             //console.log(cartTotal)
             return cartTotal
@@ -82,24 +35,42 @@ const reducer = (state, action) => {
             total:0,
             amount:0
         })
-        /**this is the function that will fix digits after 
-         * the comma for the 'total' -is a float needs to 
-         * be fixed for to digits-*/
         total = parseFloat(total.toFixed(2))
 
            return { ...state, total, amount}
     }
-    /**here i copy the previous state and set loading to true */
+  
     if (action.type === 'LOADING') {
        return {...state, loading: true}
     }
-    /**here i copy the previous state, and set the cart prop
-     * as the 'action.payload' the is the 'cart' value and 
-     * switch loading to 'false'*/
+
     if (action.type === 'DISPLAY_ITEMS') {
        return {...state, cart: action.payload, loading:false}
     }
-    return state
+    /**Building toggleAmount condition */
+    if (action.type === 'TOGGLE_AMOUNT') {
+        /**i build a tempCart as the previous implementation */
+        let tempCart = state.cart.map((cartItem) => {
+            /**'cartItem.id' match to 'action.payload.id'
+             * ----data id--       -----user action------ */
+            if (cartItem.id === action.payload.id) {
+                /**then if macth, i toogle betwwen 'increment' 
+                 * and 'decrease' */
+                if (action.payload.type === 'inc') {
+                    return {...cartItem, 
+                               amount:cartItem.amount + 1 }
+                }
+                if (action.payload.type === 'dec') {
+                    return {...cartItem, 
+                               amount:cartItem.amount - 1 }
+                }
+            }
+            return cartItem
+        })
+        .filter((cartItem) => cartItem.amount !== 0)
+        return {...state, cart: tempCart}
+    }
+    throw new Error('no matching action type')
 }
 
 export default reducer;
