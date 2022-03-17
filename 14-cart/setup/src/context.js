4f1b5f35beb759f13ee9facecb42f4582119f0b3
Erick Rodriguez <erickrod54@gmail.com>
2022-03-16 20:53:53 -0400

    Cart app version 7 - context js - Features: Implementing fetching data to get cart data from the API, Building LOADING and DISPLAY_ITEMS action, Implementing a second useEffect to make a call for API

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -2,15 +2,21 @@ import React, { useState, useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
 import reducer from './reducer'
 
-/**Cart app version 6  - context js - Features:
- *            ---> Implementing 'GET_TOTALS' feature
- *                 to get the total of the amounts.    
+/**Cart app version 7  - context js - Features:
+ *            ---> Implementing fetching data to get
+ *                 cart data from the API:
  * 
- * Note: this action is going to be build on reducer js,
- * this 'totals' thanks to this 'useEffect' will be
- * show in the shopping bag on the Navbar
+ *                  -->Building action for 'LOADING'.
+ *                  -->Building action for 'DISPLAY_ITEMS'.
+ * 
+ *            ---> Implementing a second useEffect
+ *                 to make a call for the API.
+ *  
+ * Note: this implementation will substitute the 'data js'
+ * 
  *  */
 
+/**here i have the API url that contain the data */
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
@@ -38,12 +44,28 @@ const increase = (id) => {
   dispatch({type:'INCREASE', payload: id})
 }
 
-/**useEffect make able to set as dependency 'state.cart'
- * to track the change on the car and build 'GET_TOTALS' :
- * 
- *  >> this 'totals' thanks to this 'useEffect' will be
- *  >> show in the shopping bag on the Navbar
- * */
+/**here i build a fecthData for 'cart', 
+i made use of promises to fecth data */
+const fetchData = async () => {
+  /**the action for the loading */
+  dispatch({type:'LOADING'});
+
+  /**the response to fecth the url */
+  const response = await fetch(url);
+
+  /**the cart with the response converted to json */
+  const cart = response.json();
+
+  /**the action to display the items */
+  dispatch({type:'DISPLAY_ITEMS', payload:cart})
+
+}
+
+/**Here i build the useEffect to display the data */
+useEffect(() => {
+  fetchData()
+},[])
+
 useEffect(() => {
 //i can test it this way: console.log('hello world')
   dispatch({type:'GET_TOTALS'})
