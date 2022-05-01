1473016771ddc0c71e591da8e1f1a4aa94336214
Erick Rodriguez <erickrod54@gmail.com>
2022-05-01 10:42:52 -0400

    Cocktails app version 1 - About Component - Features: Building basic structure for About Component

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
+/**Cocktails app version 1 - 'About' Component - Features:
+ * 
+ *      -->Building basic structure for 'About' Component.
+ * 
+ * Note: This component is going to be customized later
+ */
+
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Consectetur proident eiusmod irure elit cillum anim sit
+         culpa sunt ut aliqua. Ullamco id duis excepteur ipsum esse culpa anim id ad ex. 
+         Anim reprehenderit nisi magna non magna ex fugiat sint enim veniam esse irure aliquip culpa. 
+        Ex cupidatat ullamco adipisicing aute pariatur Lorem proident 
+        excepteur adipisicing elit. Aliqua voluptate amet fugiat nostrud.
+        Laborum mollit ea adipisicing qui. Non quis cillum velit proident sint cillum
+         tempor cillum sunt. Dolor Lorem commodo ex nulla est non ea consequat nostrud ad cupidatat culpa. Nostrud enim adipisicing duis ut elit ut ut mollit. </p>
+    </section>
   )
 }
 
