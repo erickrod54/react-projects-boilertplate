1f2aaae77afa7d32839aab8897ec9c2f9a8abdbd
Erick Rodriguez <erickrod54@gmail.com>
2022-05-01 10:37:30 -0400

    Cocktails app version 1 - NavBar Component - Features: Setting the logo as a link to take back home, Placing Links to navigate to home and about

--- 15-cocktails/setup/src/components/Navbar.js
+++ 15-cocktails/setup/src/components/Navbar.js
@@ -2,11 +2,38 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../logo.svg'
 
+/**Cocktails app version 1 - 'NavBar' Component - Featrues:
+ * 
+ *      -->Setting the 'logo' as a Link to take back 'Home'.
+ * 
+ *      -->Placing links to navigate to 'home' and 'about'.
+ * 
+ * Note: These componnents are going to be rendered for 
+ * the home page
+ */
+
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          <img src={logo} alt='cocktail db logo' className='logo'/>
+        </Link>
+      </div>
+      <ul className='nav-links'>
+        <li>
+          <Link to='/'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/about'>
+            About
+          </Link>
+        </li>
+      </ul>
+     
+    </nav>
   )
 }
 
