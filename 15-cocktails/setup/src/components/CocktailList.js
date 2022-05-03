bef0fc7b486ff958ef1be5c7115208e1bad1f656
Erick Rodriguez <erickrod54@gmail.com>
2022-05-02 20:33:29 -0400

    Cocktails app version 2 - CocktailsList Component - Features: Getting the fetching result of cocktails form the provider

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -3,7 +3,35 @@ import Cocktail from './Cocktail'
 import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
+/**Cocktails app version 2 - 'CocktailsList' Component - Featrues:
+ * 
+ *      -->Getting the fetching result of cocktails
+ *         from the provider.
+ * 
+ * Notes: the conditions are made to handle conditions for the
+ * search behavior that concerns to this component
+ */
+
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext()
+  /**here i just console log it to check that i'm
+   * getting the value from the provider
+   */
+  console.log(cocktails)
+  if (loading) {
+    return <Loading />
+  }
+
+  if (cocktails.length < 1 ) {
+    return(
+    <>
+    <h2 className='section-title'>
+      no cocktails matches with your criteria
+    </h2>
+    </>  
+    )
+  }
+
   return (
     <div>
       <h2>cocktail list component</h2>
