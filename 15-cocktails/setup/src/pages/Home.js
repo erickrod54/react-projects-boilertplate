2da093a7ea9ce36def380a9ca55d95bad80dc76b
Erick Rodriguez <erickrod54@gmail.com>
2022-05-02 20:38:53 -0400

    Cocktails app version 1 - Home page - Features: Importing and Placing SearchForm, Importing and Placing CocktailsList

--- 15-cocktails/setup/src/pages/Home.js
+++ 15-cocktails/setup/src/pages/Home.js
@@ -2,11 +2,21 @@ import React from 'react'
 import CocktailList from '../components/CocktailList'
 import SearchForm from '../components/SearchForm'
 
+/**Cocktails app version 1 - 'Home' page - Featrues:
+ * 
+ *      -->Importing and placing 'SearchForm'.
+ * 
+ *      -->Importing and placing 'CocktailsList'.
+ * 
+ * Note: These componnents are going to be rendered for 
+ * the home page
+ */
 const Home = () => {
   return (
-    <div>
-      <h2>home page</h2>
-    </div>
+    <main>
+      <SearchForm />
+      <CocktailList />
+    </main>
   )
 }
 
