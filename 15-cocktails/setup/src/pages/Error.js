1b805da81945d7c0ab02f352b8572e545040011d
Erick Rodriguez <erickrod54@gmail.com>
2022-05-03 17:34:20 -0400

    Cocktails app version 1 - Error Page - Features: Setting a basic structure for Error Component

--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
+/**Cocktails app version 1 - 'Error' page - Featrues:
+ * 
+ *      -->Setting a basic structure for 'Error' Component.
+ *      
+ * 
+ * Note: This component has displaying a message and a link to take
+ * back home.
+ */
+
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
+    <section className='error-page'> 
+      <div className='error-container'>
+        <h1>oops! it's a dead end</h1>
+        <Link to='/' className='btn btn-primary'>
+          back home
+        </Link>
+      </div>
+    </section>
   )
 }
 
