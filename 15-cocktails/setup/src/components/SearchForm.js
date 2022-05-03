bac3885edbb2a44fdf2f02023d35bf6ea3e6802d
Erick Rodriguez <erickrod54@gmail.com>
2022-05-02 20:30:04 -0400

    Cocktails app version 1 - SearchFrom Component - Features: Destructuring setSearchTerm value using the provider useGlobalContext, testing also cocktails datalog in it from the Provider

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { useGlobalContext } from '../context'
 
+/**Cocktails app version 1 - 'SearchForm' Component - Featrues:
+ * 
+ *        --> Destructuring 'setSearchTerm' value using 
+ *          the provider 'useGlobalContext'.
+ * 
+ * Notes: every time that i'm typing is the 'setSearchTerm' 
+ * functionality is going to be called
+ */
+
 const SearchForm = () => {
+  /**here i destructure setSearchTerm */
+  const { setSearchTerm, cocktails } = useGlobalContext();
+  console.log(cocktails)
+  
   return (
     <div>
       <h2>search form component</h2>
