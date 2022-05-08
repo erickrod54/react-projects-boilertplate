547a0e43af7f56add9d51bfd2b5293c28844c38c
Erick Rodriguez <erickrod54@gmail.com>
2022-05-07 23:27:26 -0400

    Cocktails app version 3 - Cocktail Component - Fetaures: Destructing and rendering props related with the single cocktails from the API

--- 15-cocktails/setup/src/components/Cocktail.js
+++ 15-cocktails/setup/src/components/Cocktail.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-const Cocktail = () => {
+/**Cocktails app version 3 - 'Cocktail' Component - Features:
+ * 
+ *      --> Destructuring and rendering props related with
+ *          the single cocktail from the API.
+ * 
+ * Notes: this is the single cocktail Component
+ */
+const Cocktail = ({ image, name, id, info, glass }) => {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
+    <>
+    <article className='cocktail'>
+      <div className='img-container'>
+        <img src={image} alt={name}/>
+      </div>
+      <div className='cocktail-footer'>
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <h5>{info}</h5>
+      </div>
+    </article>
+    </>
   )
 }
 
