8b19abc7fdbd8d81c8cb22e9841ae59da5137f8a
Erick Rodriguez <erickrod54@gmail.com>
2022-05-10 22:12:05 -0400

    Cocktails app version 5 - SingleCocktail Component - Features: Building state for loading and cocktail, Implementing useEffect to build a function to get the cocktail data

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -3,10 +3,46 @@ import Loading from '../components/Loading'
 import { useParams, Link } from 'react-router-dom'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
+/**Cocktails app version 5 - 'SingleCocktail' Component - 
+ * Features:
+ * 
+ *      --> Building state for 'loading' and 'cocktail'.
+ * 
+ *      --> Implementing 'useEffect' to build a function
+ *          to get the 'cocktail' data
+ * 
+ * Notes: This component will handle the cocktail at 'detail'
+ * level, the 'Link' previously set on Cocktail address to this
+ * Component.
+ */
+
 const SingleCocktail = () => {
+  /**here i build the states */
+  const { id } = useParams()
+  const [loading, setLoading ] = React.useState(false)
+  const [ cocktail, setCocktail ] = React.useState(null)
+  
+  React.useEffect(() => {
+    /**i set loading state to 'true' */
+    setLoading(true);
+    /**i start to build the function to get the data */
+    async function getCocktail(){
+      /**i use a try catch to get the data */
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json()
+        /**the response will be the original object */
+        console.log(data)
+      } catch (error) {
+        
+      }
+    }
+    getCocktail()
+  }, [id])
   return (
     <div>
-      <h2>single cocktail page </h2>
+      {/**here i get the id */}
+      <h2>{id}</h2>
     </div>
   )
 }
