cf56f7409278db68956529a9701b5637adb3d2ab
Erick Rodriguez <erickrod54@gmail.com>
2022-05-10 21:07:55 -0400

    Cocktails app version 5 - SearchForm Component - Features: Building the SearchValue feature to get render as result the values typed by the user, Implementing useEffect to focus on the input when the app is mounting

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useGlobalContext } from '../context'
 
-/**Cocktails app version 1 - 'SearchForm' Component - Featrues:
+
+/**Cocktails app version 5 - 'SearchForm' Component - Features:
  * 
- *        --> Destructuring 'setSearchTerm' value using 
- *          the provider 'useGlobalContext'.
+ *        --> Building the 'SearchValue' feature to get render
+ *            as result the values typed by the user.
+ * 
+ *        --> Implementing 'useEffect' to focus on the input
+ *            when the app is mounting.
  * 
  * Notes: every time that i'm typing is the 'setSearchTerm' 
  * functionality is going to be called
@@ -13,12 +17,30 @@ import { useGlobalContext } from '../context'
 const SearchForm = () => {
   /**here i destructure setSearchTerm */
   const { setSearchTerm, cocktails } = useGlobalContext();
-  console.log(cocktails)
-  
+  const searchValue = React.useRef('');
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+  /**here i focus in the input on app mounting */
+  React.useEffect(() => {
+    searchValue.current.focus()
+  })
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form action='' className='search-form'>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input 
+            type='text' 
+            id='name' 
+            ref={searchValue}
+            onChange={searchCocktail}
+            />
+        </div>
+      </form>
+    </section>
   )
 }
 
