c121265554f010b272d00bb6bebe83447ed353e5
Erick Rodriguez <erickrod54@gmail.com>
2022-05-13 21:15:10 -0400

    Markdown app version 1 - App js file - Features: Building Markdown app initial setup, Importing and Placing MarkdownWrapper to style Markdown app

--- 16-markdown-preview/setup/src/App.js
+++ 16-markdown-preview/setup/src/App.js
@@ -1,8 +1,31 @@
 import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
+import { MarkDownWrapper } from './components/styled.components'
+
+/**Markdown app version 1 - 'App' js file - Features:
+ * 
+ *      -->Building 'Markdown app' initial setup.
+ * 
+ *      -->Importing and Placing 'MarkDownWrapper' to
+ *         style 'Markdown app'.
+ * 
+ * Note: This naviagation is made using now is using 
+ * react-router version 6
+ */
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [ markdown, setMarkdown ] = useState('## markdown preview')
+  return(
+    <MarkDownWrapper>
+      <textarea 
+        className='input' 
+        value={markdown} 
+        onChange={(e) => setMarkdown(e.target.value)}>
+        </textarea>
+        <article className='result'>{markdown}</article>
+    </MarkDownWrapper>
+      
+     )
 }
 
 export default App
