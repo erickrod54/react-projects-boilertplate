3061d899bf865d57bfb5e6e438ef6a9d486f3c6d
Erick Rodriguez <erickrod54@gmail.com>
2022-05-14 21:24:51 -0400

    Markdown app version 2 - styled.components js file - Features: Adding Styles for headings

--- 16-markdown-preview/setup/src/components/styled.components.js
+++ 16-markdown-preview/setup/src/components/styled.components.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 
-/**Markdown app version 1 - 'styled.component' js file - 
+/**Markdown app version 2 - 'styled.component' js file - 
  * Features:
  * 
- *      -->Building'MarkDownWrapper' to
- *         style 'Markdown app'.
+ *      -->Adding Styles for 'headings'
  * 
- * Note: This 'MarkDownWrapper' will wrap the 'markdown' app.
+ * Note: also can be added a 
+ * custom style for every element that goes
+ * throught the 'ReactMarkdown' pipeline
  */
 
 export const MarkDownWrapper = styled.section`
@@ -15,6 +16,27 @@ export const MarkDownWrapper = styled.section`
   display: flex;
   flex-direction: column;
   row-gap: 4rem;
+
+  h2,
+  h3,
+  h4 {
+  letter-spacing: var(--spacing);
+  text-transform: capitalize;
+  line-height: 1.25;
+  margin-bottom: 0.75rem;
+  } 
+  h1 {
+    font-size: 2.5rem;
+  }
+  h2 {
+  font-size: 2rem;
+  }
+  h3 {
+  font-size: 1.25rem;
+  }
+  h4 {
+  font-size: 0.875rem;
+}
 
 .input {
   border-color: transparent;
