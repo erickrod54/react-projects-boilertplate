caa3590277a952f926c7a59208482853e2b5c95a
Erick Rodriguez <erickrod54@gmail.com>
2022-05-16 21:39:20 -0400

    Pagination app version 1 - Follower Component - Features: Destructuring the props that i need from API, Building a card style div to keep the props previously destructured

--- 18-pagination/setup/src/Follower.js
+++ 18-pagination/setup/src/Follower.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
-const Follower = () => {
-  return <h2>follower component</h2>
+/**Pagination app version 1 - 'Follower' Component - Features:
+ * 
+ *      --> Destructuring the props that i need form the API.
+ * 
+ *      --> Building a 'card' style div to keep the 'props'
+ *          previously destructured. 
+ * 
+ * Note: This Component is rendered in 'App' js.
+ * 
+ */
+const Follower = ({ avatar_url, html_url,login }) => {
+  return(
+    <>
+      <article className='card'>
+        <img src={avatar_url} alt={login}/>
+        <h4>{login}</h4>
+        <a href={html_url} className='btn'>
+          view profile
+        </a>
+      </article>
+    </>
+  )
 }
 
 export default Follower
