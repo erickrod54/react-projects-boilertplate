2c037d8a8c989d1c4c82ea9a993052a82e417dca
Erick Rodriguez <erickrod54@gmail.com>
2022-05-17 00:20:40 -0400

    Pagination app version 1 - useFetch js file - Features: Piping data from the custom Fetch to paginate function in utils

--- 18-pagination/setup/src/useFetch.js
+++ 18-pagination/setup/src/useFetch.js
@@ -2,6 +2,15 @@ import { useState, useEffect } from 'react'
 import paginate from './utils'
 const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
 
+/**Pagination app version 1 - 'useFetch' js file - Features:
+ * 
+ *      --> Piping 'data' from the custom Fetch to 'paginate'
+ *          function in utils.
+ * 
+ * Note: 'useFetch()' hook fetch already the data from
+ * the github API.
+ * 
+ */
 export const useFetch = () => {
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState([])
@@ -9,6 +18,7 @@ export const useFetch = () => {
   const getProducts = async () => {
     const response = await fetch(url)
     const data = await response.json()
+    paginate(data)
     setData(data)
     setLoading(false)
   }
