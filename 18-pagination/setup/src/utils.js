e0fc475139f75bc4ead58ed0de005b69ba8bb641
Erick Rodriguez <erickrod54@gmail.com>
2022-05-17 00:22:36 -0400

    Pagination app version 1 - App js file - Features: Testing the data already piped from useFetch Custom hook

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,16 @@
-const paginate = () => {}
+/**Pagination app version 1 - 'App' js file - Features:
+ * 
+ *      --> Testing the data already piped from 'useFetch'
+ *          Custom hook.
+ * 
+ * Note: The porpouse of utils js is to build and array of
+ * arrays.
+ * 
+ * i already have an array that comes from the API and i will 
+ * build and array to set a limit of 'followers' per pagination
+ */
+const paginate = (followers) => {
+    console.log('log from utils js', followers)
+}
 
 export default paginate
