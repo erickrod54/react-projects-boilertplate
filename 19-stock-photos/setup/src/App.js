72cdcdbf59afbbf4e0cf59456bf912bfa76f6095
Erick Rodriguez <erickrod54@gmail.com>
2022-05-18 23:07:00 -0400

    Stock-photos app version 1 - App js file - Features: Building sattes for loading and photos, Building useEffect to invoke fetchImages, Building fetchImages to get the data from the API

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -5,7 +5,47 @@ import Photo from './Photo'
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
+/**Stock-photos app version 1 - 'App' js file - 
+ * Features:
+ * 
+ *      --> Building states for 'loading' and 'photos'.
+ * 
+ *      --> Building 'useEffect' to invoke 'fetchImages'.
+ * 
+ *      --> Building 'fetchImages' to get the data
+ *          from the API.
+ * 
+ * Note: there is an issue when i use 'mainUrl'
+ * as string template to construct the 'url'
+ * pending to test it
+ * 
+ * */
 function App() {
+
+  /**Here i build the states */
+  const [loading, setLoading ] = useState(false);
+  const [photos, setPhotos ] = useState([]);
+
+  /**here i build 'fetchImages' */
+  const fetchImages = async() => {
+        setLoading(true)
+        let url;
+        url = 'https://api.unsplash.com/photos/?client_id=ORS8bvDrLdlvnckQexhtFqRY-tGsxtDcx-iaICjygOk'
+        try {
+          const response = await fetch(url);
+          const data = response.json();
+          /**i prompted to test that i get the data */
+          console.log(data)
+        } catch (error) {
+          setLoading(false)
+          console.log(error)
+        }
+  }
+  /**here i Build 'useEffect' to invoke 
+   * fetchImages*/
+  useEffect(() => {
+    fetchImages()
+  },[])
   return <h2>stock photos starter</h2>
 }
 
