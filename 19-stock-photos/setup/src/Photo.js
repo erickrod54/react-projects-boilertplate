2b22f699fab7724e5b1c011e419a3ec1ba8472c9
Erick Rodriguez <erickrod54@gmail.com>
2022-05-19 22:58:05 -0400

    Stock-photos app version 4 - Photo Component - Features: Destructuring the image in the props that i need to display, Building a basic form to return them

--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,34 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+/**Stock-photos app version 4 - 'Photo' Component file - 
+ * Features:
+ * 
+ *      --> Destructuring the 'image' in the 
+ *          props that i need to display.
+ * 
+ * Note: this is the 'Photo' Component previously
+ * called from 'App' js
+ * 
+ * */
+
+/**here i destructure the props from each 'photo' */
+const Photo = ({ urls:{regular}, alt_description, likes, user:{ name, portfolio_url, profile_image: { medium }}}) => {
+  return(
+    <>
+      <article className='photo'>
+        {/**here i render the 'img' */}
+        <img src={regular} alt={alt_description} />
+        {/**here a container with 'photo-info'*/}
+        <div className='photo-info'>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        <a href={portfolio_url}>
+          <img src={medium} alt={name} className='user-img'/>
+        </a>
+        </div>
+      </article>
+    </>
+  )
 }
 
 export default Photo
