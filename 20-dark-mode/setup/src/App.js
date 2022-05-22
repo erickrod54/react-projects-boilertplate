3af47ecdaf98bf30ca1f462d6fc9bdfd49e2faf6
Erick Rodriguez <erickrod54@gmail.com>
2022-05-22 19:30:40 -0400

    Dark-mode app version 1 - App js file - Features: Building nav section for the title -1- and the button toggle, Building a section to map the data on Article Component

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -2,8 +2,37 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+/**Dark-mode app version 1- 'App' js file - Features: 
+ * 
+ *      -->Building nav section for the 'title' -h1- and
+ *        the button 'toogle'.
+ * 
+ *      -->Building a section to map the 'data' on 
+ *         'Article' Component.
+ * 
+ * Note:This file will hold the two main sections. 
+ * 
+ * */
 function App() {
-  return <h2>dark mode starter</h2>
+  return(
+    <>
+    {/**here i place the nav section*/}
+      <main>
+        <nav>
+          <div className='nav-center'>
+            <h1>overreacted</h1>
+            <button className='btn'>toggle</button>
+          </div>
+        </nav>
+        {/**here i map the data on the 'Article' Component */}
+        <section className='articles'>
+          {data.map((article) => {
+            return <Article key={article.id} {...article}/>
+          })}
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
