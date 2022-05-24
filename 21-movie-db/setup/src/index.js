9a33cb0eb1b002868e5c58d0b831fd97ea76b469
Erick Rodriguez <erickrod54@gmail.com>
2022-05-24 10:36:04 -0400

    Movie-db app version 1 - index js file - Features: Implementing BrowserRouter and wrapping the App

--- 21-movie-db/setup/src/index.js
+++ 21-movie-db/setup/src/index.js
@@ -4,11 +4,24 @@ import './index.css'
 import App from './App'
 import { AppProvider } from './context'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+/**Movie-db app version 1 - 'index' js file - Features: 
+ * 
+ *      -->Implementing 'BrowserRouter' 
+ * 
+ * Note: I use an alias of Route, wrapping with BrowserRouter
+ * here has the same functionality than doing it at App js,
+ * the difference will be that the code will look more 
+ * cleaner and the Routing will come from the root directory
+ * */
+
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <Router>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
