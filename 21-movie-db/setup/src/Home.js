5d122f10ce8a19a06b50b8650875420a266252e7
Erick Rodriguez <erickrod54@gmail.com>
2022-05-24 10:37:30 -0400

    Movie-db app version 1 - Home Component - Features: Importing and Placing Form Component, Importing and Placing Movies Component

--- 21-movie-db/setup/src/Home.js
+++ 21-movie-db/setup/src/Home.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import Form from './SearchForm'
 import Movies from './Movies'
+
+/**Movie-db app version 1 - 'Home' Component - Features: 
+ * 
+ *      -->Importing and Placing 'Form' Component.
+ * 
+ *      -->Importing and Placing 'Movies' Component.
+ * 
+ * Note: 'Form' Component will be for a search feature,
+ * and 'Movies' Component will be to display all the movies. 
+ * */
+
 const Home = () => {
-  return <h2>movie component</h2>
+  return(
+    <>
+      <main>
+        <Form />
+        <Movies />
+      </main>
+    </>
+  )
 }
 
 export default Home
