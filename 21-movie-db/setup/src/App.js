abf2173ed76da6f5435163d5dfa62c1451927edc
Erick Rodriguez <erickrod54@gmail.com>
2022-05-24 10:31:08 -0400

    Movie-db app version 1 - App js file - Features: Building Routing feature, Building Route for Home where all the movies will be located, Building a children Route for a Movie -single movie- Component

--- 21-movie-db/setup/src/App.js
+++ 21-movie-db/setup/src/App.js
@@ -4,8 +4,31 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Movie from './SingleMovie'
 
+/**Movie-db app version 1 - 'App' js file - Features: 
+ * 
+ *      -->Building 'Routing' feature
+ * 
+ *      -->Building Route for 'Home' where
+ *         all the movies will be located.
+ * 
+ *      -->Building a 'children' Route for 
+ *        a 'Movie' -single movie- Component
+ * 
+ * Note: This Routing feature is based on 'react-router-dom'
+ * version 5 - later i'll update to version 6
+ * */
+
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/movies/:id' children={<Movie />}/>
+      </Switch>
+    </>
+  )
 }
 
 export default App
