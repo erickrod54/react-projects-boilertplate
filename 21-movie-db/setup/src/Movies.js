7c608cade56cd1f1f38c7c4291a3054916c650fe
Erick Rodriguez <erickrod54@gmail.com>
2022-05-25 08:54:29 -0400

    Movie-db app version 2 - Movie Component - Features: Destructuring movies, isLoading using the useGlobalContext hook, Setting up loading state to display the loading state, Mapping the movies data and render it

--- 21-movie-db/setup/src/Movies.js
+++ 21-movie-db/setup/src/Movies.js
@@ -4,8 +4,64 @@ import { Link } from 'react-router-dom'
 const url =
   'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
 
+  /**Movie-db app version 2 - 'Movies' Component - Features: 
+ * 
+ *      --> Destructuring 'movies', 'isLoading' using 
+ *          the 'useGlobal' context hook.
+ * 
+ *      --> Settig up 'loading' state to display the 
+ *          'loading' state.
+ * 
+ *      --> Mapping the 'movies' data and render it.
+ * 
+ * Note: Now being doing this i can use these values provided
+ * everywhere where i want in the app
+ * 
+ * 'object not iterable' is because instead of  destruturing
+ * it i do it as an arrays and thats wrong, 'usingGlobalContext'
+ * always 'destructure'
+ * */
+
 const Movies = () => {
-  return <h2>movies component</h2>
+
+  /**Here i destructure 'movies', and 'isLoading' */
+  const { movies, isLoading } = useGlobalContext();
+
+  /**here  i set the isLoading */
+  if (isLoading) {
+      return <div className='loading'/>
+  }
+  /**here i map the movie data */
+  return(
+    <>
+      <section className='movies'>
+        {movies.map((movie)=>{
+          //console.log(movie)
+          /**here i convert the prop to friendly format */
+          const { imdbID: id, 
+            Poster: poster, 
+            Title: title, 
+            Year: year } = movie;
+
+          /**here i render the movie props */
+          return(
+            <>
+              <Link to={`/movies/${id}`} key={id} className='movie'>
+                <article>
+                  <img src={poster === 'N/A' ? url : poster} alt={title}/>
+                  <div className='movie-info'>
+                    <h4 className='title'>{title}</h4>
+                    <p>{year}</p>
+                  </div>
+                </article>
+              </Link>
+
+            </>
+            )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default Movies
