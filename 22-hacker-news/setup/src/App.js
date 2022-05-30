04a5b7d9e3837336bd0bb6325b74454aafe01258
Erick Rodriguez <erickrod54@gmail.com>
2022-05-30 19:21:17 -0400

    Hacker-news app version 1 - App js file - Features: Importing and Placing SearchForm, Stories and Buttons Components

--- 22-hacker-news/setup/src/App.js
+++ 22-hacker-news/setup/src/App.js
@@ -2,8 +2,40 @@ import React from 'react'
 import SearchForm from './SearchForm'
 import Stories from './Stories'
 import Buttons from './Buttons'
+
+/**Hacker-news app version 1 - 'App' js file - Features: 
+ * 
+ *      -->Importing and Placing 'SearchForm', 'Stories' and
+ *         'Buttons' Components.
+ * 
+ * Note: 
+ * 
+ * api reference:
+ * 
+ *    https://hn.algolia.com/api
+ * 
+ * API end-points: 
+ * 
+ *  http://hn.algolia.com/api/v1/search?query=
+ * 
+ * to test it:
+ * 
+ * http://hn.algolia.com/api/v1/search?query=svelte
+ * 
+ * as example for pagination:
+ * 
+ * http://hn.algolia.com/api/v1/search?query=svelte&page=4
+ * 
+ * */
+
 function App() {
-  return <h2>hacker news starter</h2>
+  return(
+    <>
+      <SearchForm />
+      <Stories />
+      <Buttons />
+    </>
+  )
 }
 
 export default App
