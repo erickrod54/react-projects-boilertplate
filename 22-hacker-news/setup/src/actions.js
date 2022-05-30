cc85f7a6f46699148534be933de47214ab4c5ad8
Erick Rodriguez <erickrod54@gmail.com>
2022-05-30 19:22:26 -0400

    Hackers-news app version 1 - actions js file - Features: Building actions

--- 22-hacker-news/setup/src/actions.js
+++ 22-hacker-news/setup/src/actions.js
@@ -1,3 +1,17 @@
+/**Hacker-news app version 1 - 'actions' js file - Features: 
+ * 
+ *      -->Building actions.
+ * 
+ * Note: This implementation is very important because building
+ * actions like are here i can avoid silly typo errors, and get
+ * focus on functionallity. 
+ * 
+ * So this import for the actions variables must be when i'm
+ * dispatching the action, this case 'context' and 'Stories'
+ * but can be imported and use everywhere where i need it in
+ * the app
+ * */
+
 export const SET_LOADING = 'SET_LOADING'
 export const SET_STORIES = 'SET_STORIES'
 export const REMOVE_STORY = 'REMOVE_STORY'
