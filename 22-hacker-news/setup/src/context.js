e8a529d6c52ae7e2783e4fc26d498fc921277eaf
Erick Rodriguez <erickrod54@gmail.com>
2022-05-29 21:34:27 -0400

    Hacker-news app version 1 - context js file - Features: Building state for the reducer actions, Building initalState with the first action isLoading, Building fetchStories to make a request for the API data, Building the useEffect to invoke fetchStories, Spreading as value the reducer state

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -1,4 +1,26 @@
 import React, { useContext, useEffect, useReducer } from 'react'
+/**Hacker-news app version 1 - 'context' js file - Features: 
+ * 
+ *      --> Building state for the 'reducer' actions.
+ * 
+ *      --> Building 'initialState' with the first 
+ *          action 'isLoading'.
+ * 
+ *      --> Building 'fetchStories' to made the request
+ *          for the API data.
+ * 
+ *      --> Building the 'useEffect' to invoke 'fetchStories'. 
+ * 
+ *      --> Spreading as 'value' the reducer state.    
+ * 
+ * Note: the first action that is going to be spread is the
+ * 'SET_LOADING'
+ * 
+ * So this import for the actions variables must be when i'm
+ * dispatching the action, this case 'context' and 'Stories'
+ * but can be imported and use everywhere where i need it in
+ * the app
+ * */
 
 import {
   SET_LOADING,
@@ -11,12 +33,30 @@ import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
-const initialState = {}
+const initialState = {
+  isLoading:true,
+}
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  /**here i build the state for the reducer using
+   * 'useReducer' hook*/
+  const [ state, dispatch ] = useReducer(reducer, initialState)
+
+  /**here i build 'fetchStories' request to request 
+   * the 'SET_LOADING' action*/
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING})
+  }
+
+  /**here i use 'useEffect' hook to invoke it */
+  useEffect(() => {
+    fetchStories()
+  },[])
+  
+  /**here in the provider i spread the reducer state as value */
+  return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
