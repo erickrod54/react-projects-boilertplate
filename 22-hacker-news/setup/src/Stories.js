b14b1ad6d6a48f262b16284c2773b273fb177137
Erick Rodriguez <erickrod54@gmail.com>
2022-05-29 21:36:34 -0400

    Hacker-news app version 1 - Stories js file - Features: Destructuring isLoading prop using the useGlobalContext, Building a conditional rendering displaying a div with the className loading

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
 import { useGlobalContext } from './context'
+/**Hacker-news app version 1 - 'Stories' js file - Features: 
+ * 
+ *      --> Destructuring 'isLoading' prop using the
+ *          'useGlobalContext'.
+ *  
+ *      --> Building a conditional rendering displaying
+ *          a div with the className 'loading'.
+ * 
+ * Note: for the moment i'll only display the loading for the
+ * Stories component.
+ * 
+ * */
 
 const Stories = () => {
+  const { isLoading } = useGlobalContext();
+
+  if (isLoading) {
+    return <div className='loading'/>
+  }
   return <h2>stories component</h2>
 }
 
