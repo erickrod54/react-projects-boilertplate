344376b7d0c1944efcda5dd3da5141f971c76c55
Erick Rodriguez <erickrod54@gmail.com>
2022-06-03 12:28:27 -0400

    Hacker-news app version 7 - Buttons Component - Features: Building prev and next button

--- 22-hacker-news/setup/src/Buttons.js
+++ 22-hacker-news/setup/src/Buttons.js
@@ -1,8 +1,39 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 
+/**Hacker-news app version 7 - 'Buttons' Component - Features: 
+ * 
+ *      -->Building 'prev' and 'next' button.
+ *          
+ * Note: the references for the styles in the global stylesheet
+ * 
+ * */
+
 const Buttons = () => {
-  return <h2>button container</h2>
+  /**here i destructure the values to make 
+   * pagination functionality */
+  const { isLoading, page, nbPages, handlePage } = useGlobalContext()
+  
+  return(
+    /**button container */
+    <div className='btn-container'>
+      {/**this is the 'Prev' button */}
+      <button 
+        disabled={isLoading} 
+        onClick={() => handlePage('dec')}>
+          prev
+        </button>
+
+        <p>some values</p>
+
+        {/**this is the 'Next' button */}  
+        <button 
+        disabled={isLoading} 
+        onClick={() => handlePage('inc')}>
+          next
+        </button>
+    </div>
+  )
 }
 
 export default Buttons
