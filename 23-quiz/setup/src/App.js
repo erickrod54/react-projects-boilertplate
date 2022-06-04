9f7a14d4627e0a2faa52c2cb75ce2d5ee26f018d
Erick Rodriguez <erickrod54@gmail.com>
2022-06-04 11:38:00 -0400

    Quiz app version 1 - App js file - Features: Destructuring states from useGlobalContext, Conditionally rendering waiting state for the SetupFrom Component, Conditionally rendering loading for the loading state

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -4,8 +4,38 @@ import { useGlobalContext } from './context'
 import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
+
+/**Quiz app version 1 - 'App' js file - Features: 
+ * 
+ *      -->Destructuring states from useGlobalContext.
+ * 
+ *      -->Conditionally rendering 'waiting' state for
+ *        the 'SetupForm' Component.
+ * 
+ *      -->Conditionally rendering 'loading' for the 
+ *         loading state.
+ * 
+ *      -->Accessing to the 'question' object from the API.
+ * 
+ * Note: this version will keep the basic setup for
+ * the quiz app.
+ * 
+ * */
+
 function App() {
-  return <h2>quiz starter</h2>
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+
+  if (waiting) {
+    return <SetupForm />
+  }
+
+  if (loading) {
+    return <Loading />
+  }
+  console.log('the single question => ', questions[0])
+  return(
+    <main>quiz app</main>
+  )
 }
 
 export default App
