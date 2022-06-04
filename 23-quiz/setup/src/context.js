a678e307ea0b2f3525c7076505b2b262bf6a8e98
Erick Rodriguez <erickrod54@gmail.com>
2022-06-04 11:41:22 -0400

    Quiz app version 1 - context js file - Features: Building the states for the Quizz app, Building fetchQuestions to get the data back from the API, Building useEffect in order to invoke the fetch request

--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -1,6 +1,21 @@
 import axios from 'axios'
 import React, { useState, useContext, useEffect } from 'react'
 
+/**Quiz app version 1 - 'context' js file - Features: 
+ * 
+ *      -->Building the states for the Quiz app.
+ * 
+ *      -->Building 'fetchQuestions' to get the data
+ *         back from the API.
+ * 
+ *      -->Building 'useEffect' in order to invoke
+ *         the fetch request.
+ * 
+ * Note: the request is made using 'axios' library
+ * in order to friendly handle the API concerns
+ * 
+ * */
+
 const table = {
   sports: 21,
   history: 23,
@@ -11,10 +26,72 @@ const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
 
+const tempUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  /**this is for the form */
+  const [ waiting, setWaiting ] = useState(true);
+  /**this is for fetch data from the API */
+  const [ loading, setLoading ] = useState(false);
+  /**here the state for the questions value */
+  const [ questions, setQuestions ] = useState([]);
+  /**the index for the questions*/
+  const [ index, setIndex ] = useState(0);
+  /**here is the state for the number of correct questions */
+  const [ correct, setCorrect ] = useState(0)
+  /**here is the state for error */
+  const [ error, setError ] = useState(false)
+  /**this is the state for modal to show results */
+  const [ isModalOpen, setIsModalOpen ] = useState(false)
+
+  /**here i build the fetchQuestions -for now
+   * i target 'url' that in this version is empty, i'll use
+   * temporal data 'tempUrl'- */
+  const fetchQuestions = async(url) => {
+    setLoading(true);
+    setWaiting(false);
+
+    const response = await axios(url).
+    catch(err => console.log(err))
+
+    console.log(response)
+
+    if (response) {
+      const data = response.data.results
+      console.log('this is data => ', data)
+      /**i set the states in order to display 'quizz' app
+       * previously defined in 'App' js*/
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false)
+        setWaiting(false)
+        setError(false)
+      }else{
+        setWaiting(true)
+        setError(true)
+      }
+    }
+  }
+
+  /**here i invoke 'fetchQuestions' passing throught the 
+   * 'tempUrl' */
+  useEffect(() => {
+    fetchQuestions(tempUrl)
+  }, [])
+
+
+  return <AppContext.Provider 
+            value={{
+              waiting,
+              loading,
+              questions,
+              index,
+              correct,
+              error,
+              isModalOpen
+            }}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
